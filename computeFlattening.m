11158f14ce234f4b32171a75e90d01442a3be44d
Nikhil Trivedi <nikhiltrivedi@Nikhils-MacBook-Pro-2.local>
2024-11-16 14:32:10 -0800

    Fix Singular Matrix Issue - Compute Flattening

--- computeFlattening.m
+++ computeFlattening.m
@@ -11,7 +11,15 @@ n_eq = size(A,1);
 M=[L A'; A sparse(n_eq,n_eq)];
 rhs=[zeros(n_vars,1); b];
 warning('off','MATLAB:nearlySingularMatrix')
-x_lambda = M \ rhs;
+
+% Use Tekhonov Regularization for calculating x_lambda
+% Reference: Tikhonov, A. N., & Arsenin, V. Y. (1977). Solutions of Ill-Posed Problems. Winston & Sons.
+% addresses rank deficiency in the original M matrix
+% specify alpha: 
+% - small enough to meet error requirements
+% - large enough to make eignvalues not zero 
+alpha = 1e-10;
+x_lambda = (M' * M + alpha * speye(size(M,2))) \ (M' * rhs);
 warning('on','MATLAB:nearlySingularMatrix')
 e=max(abs(M*x_lambda-rhs));
 fprintf('error: %e\n',e);
